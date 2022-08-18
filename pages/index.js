49b2716c5855617ef49f1d993f4e918956e73bfd
PanOkurka <okurka@baby-cucumber.com>
2022-08-18 16:59:12 +0000

    feat: fixes

--- pages/index.js
+++ pages/index.js
@@ -11,28 +11,28 @@ export default function Home() {
             <motion.div className='List' exit={{y: -40, opacity: 0, transition: {duration: 0.5}}} initial={{y: 40, opacity: 0}} animate={{y: 0, opacity: 1, transition: {duration: 0.6, delay: 0.4}}}>
                 <Link id='vanilla' href='/vanilla'>
                     <a className='item'>
-                        <Image src="/images/Vanilla.webp" width='150px' height='150px' />
+                        <Image src="/images/Vanilla.webp" width='150px' height='150px' alt="Vanilla" />
                         <h1>Vanilla</h1>
                         <p>Just a simple vanilla server from Mojang</p>
                     </a>
                 </Link>
                 <Link id='servers' href='/servers'>
                     <a className='item'>
-                        <Image src="/images/servers.png" width='150px' height='150px' />
+                        <Image src="/images/servers.png" width='150px' height='150px' alt="Servers" />
                         <h1>Servers</h1>
                         <p>Server fork&apos;s of Bukkit</p>
                     </a>
                 </Link>
                 <Link id='proxy' href='/proxy'>
                     <a className='item'>
-                        <Image src="/images/proxy.png" width='250px' height='140px' />
+                        <Image src="/images/proxy.png" width='250px' height='140px' alt="Proxy" />
                         <h1>Proxy</h1>
                         <p>Proxy servers for networks</p>
                     </a>
                 </Link>
                 <Link id='modded' href='/modded'>
                     <a className='item'>
-                        <Image src="/images/modded.png" width='150px' height='150px' />
+                        <Image src="/images/modded.png" width='150px' height='150px' alt="Modded" />
                         <h1>Modded</h1>
                         <p>Some jars for modded servers</p>
                     </a>
